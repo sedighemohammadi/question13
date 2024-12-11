87e1d680c6716750a0df4e55743685d0394eb7b9
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-12-11 09:19:16 +0330

    4

---
+++ js-13-other.js
@@ -0,0 +1,24 @@
+// ? Question-1:select section counter by class name(.cards__card__img__icon) and save in variable "counterEl":
+// !Answer:
+const counterEl = document.querySelector('.cards__card__img__icon')
+
+// ? Question-2:select item counter by id(#counter) and save in variable "counter":
+// !Answer:
+const counter = document.getElementById('counter')
+
+// ? Question-3:select section (.cards) and save in variable "card":
+// !Answer:
+const card = document.querySelector('.card')
+
+// ? Question-4:select section (.cards__card) and save in variable "cards":
+// !Answer:
+const cards = document.querySelectorAll('.cards__card')
+
+// ? Question-5:select button in class cards_card_footer and save in variable "btnEl":
+// !Answer
+const btnE1 = document.querySelector('.cards_card_footer button')
+
+// ? Question-6:select the img that has attribute "man" and show in console:
+// !Answer
+const imgManAttribute = document.querySelector(' img[man] ')
+console.log(imgManAttribute);
